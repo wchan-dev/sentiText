d6c770e8a7ae3e4093f9b1be688eabb1ceb0ece4
wc-dev <chan.wilson.wc@gmail.com>
2023-08-02 15:23:33 -0700

    feat: well, proof of concept works. after 5 hrs of hacking shit together

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,31 @@
 import React, { useEffect, useState } from "react";
+import * as vader from "vader-sentiment";
 
 function App() {
   const [selectedText, setSelectedText] = useState("");
+  const [sentiment, setSentiment] = useState("");
 
   useEffect(() => {
     chrome.storage.local.get(["selectedText"], function(result) {
       setSelectedText(result.selectedText || "");
+      const intensity = vader.SentimentIntensityAnalyzer.polarity_scores(
+        result.selectedText
+      );
+      setSentiment(intensity);
     });
   }, []);
 
   return (
     <div>
       <p>Selected text: {selectedText}</p>
+      <p>Sentiment Results: </p>
+      <ul>
+        {Object.entries(sentiment).map(([key, value]) => (
+          <li key={key}>
+            {key}: {value}
+          </li>
+        ))}
+      </ul>
       {/* rest of your component */}
     </div>
   );
